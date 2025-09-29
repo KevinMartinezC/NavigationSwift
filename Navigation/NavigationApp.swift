77eba88db7055183ff8535e98766aeb5f0772f10
KevinMartinezC <ceronk003@gmail.com>
2025-09-29 13:44:07 -0600

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by KevinMartinez on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
